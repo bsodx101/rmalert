015621b96504e8efa96965cd84a07c2100814ae7
bsodx101 <49034909+bsodx101@users.noreply.github.com>
2024-11-09 01:01:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ TOKEN = os.getenv("TOKEN")
 bot = Bot(token=TOKEN)
 
 # Настройки бота
-API_KEY = os.getenv("API_KEY")  # API ключ для Redmine из окружения
-CHAT_ID = os.getenv("CHAT_ID")  # ID чата для отправки уведомлений из окружения
-REDMINE_URL = os.getenv("REDMINE_URL")  # URL Redmine из окружения
-CHECK_INTERVAL = 180  # Интервал проверки (в секундах)
-QUERY_ID = 2015  # ID вашей очереди в Redmine
+API_KEY = os.getenv("API_KEY")  
+CHAT_ID = os.getenv("CHAT_ID")  
+REDMINE_URL = os.getenv("REDMINE_URL") 
+CHECK_INTERVAL = 180  
+QUERY_ID = 2015 
 
 
 def escape_markdown_v2(text):
-    """Экранируем специальные символы для MarkdownV2."""
+    """Экранируем спецсимвлы """
     return re.sub(r'([_*.\[\]()~`>#+\-=|{}.!])', r'\\\1', text)
 
 
@@ -41,7 +41,7 @@ def get_issues(query_id):
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
-        print(f"Получены задачи: {response.json()}")  # Логируем полученные данные
+        print(f"Получены задачи: {response.json()}")  # Логгируем полученные данные
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при получении задач: {e}")
@@ -64,10 +64,10 @@ def get_last_assigned_user(issue_id):
             for detail in journal.get('details', []):
                 if detail.get('property') == 'attr' and detail.get('name') == 'assigned_to_id':
                     return journal.get('user', {}).get('name', 'Неизвестный пользователь')
-        return "Изменений не найдено"
+        return ""
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при получении журнала для задачи {issue_id}: {e}")
-        return "Ошибка при получении данных"
+        return "автор"
 
 
 # Функция для извлечения задач if priority_id in [1, 2, 3, 4]
@@ -128,7 +128,7 @@ async def track_page():
                         f"Тема: {issue['subject']}\n"
                         f"Приоритет: {priority_to_text(issue['priority_id'])}\n"
                         f"Статус: {issue['status']}\n"
-                        f"Автор: {issue['author']}\n"  # Добавляем автора задачи
+                        f"Автор: {issue['author']}\n"  
                         f"Назначил(а): {last_assigned_user}\n"
                         f"🍏 {issue['url']}\n\n"
                     )
